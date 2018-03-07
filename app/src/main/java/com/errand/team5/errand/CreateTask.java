0d35a1e25dce5007cf9397e9408b6a2a8555e2ef
Andrew Henry <ajh145@pitt.edu>
2018-03-07 16:13:29 -0500

    Update

--- app/src/main/java/com/errand/team5/errand/CreateTask.java
+++ app/src/main/java/com/errand/team5/errand/CreateTask.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +17,7 @@ public class CreateTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
         b = (Button) findViewById(R.id.create);
+        getSupportActionBar().setTitle("Create Task");
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +27,25 @@ public class CreateTask extends AppCompatActivity {
         });
     }
 
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.completed, menu);
+            return true;
+        }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.create_task:
+                setRequest();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     private void setRequest(){
         Intent returnIntent = new Intent();
