a13e04356faa6b57826781d95ec876488a341619
Andrew Henry <ajh145@pitt.edu>
2018-03-28 23:58:26 -0400

    Added firebase, changed google auth to use firebase auth, changed TaskModel, added location service

--- app/src/main/java/com/errand/team5/errand/TaskFeedAdapter.java
+++ app/src/main/java/com/errand/team5/errand/TaskFeedAdapter.java
@@ -1,6 +1,7 @@
 package com.errand.team5.errand;
 
 import android.content.Context;
+import android.location.Location;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,15 @@ import java.util.ArrayList;
 
 /**
  * Created by Andrew on 3/7/2018.
+ *
+ * Template for displaying tasks in a list view
  */
 
 public class TaskFeedAdapter extends ArrayAdapter<TaskModel> {
 
-        Context mContext;
+    private Context mContext;
     private ArrayList<TaskModel> dataSet;
+    private Location location;
 
         // View lookup cache
         private static class ViewHolder {
@@ -30,16 +34,17 @@ public class TaskFeedAdapter extends ArrayAdapter<TaskModel> {
             RatingBar creatorRating;
         }
 
-        public TaskFeedAdapter(ArrayList<TaskModel> data, Context context) {
+        public TaskFeedAdapter(ArrayList<TaskModel> data, Context context, Location currentLocation) {
             super(context, R.layout.listview_feed, data);
             this.dataSet = data;
             this.mContext=context;
+            this.location = currentLocation;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             // Get the data item for this position
-            TaskModel dataModel = getItem(position);
+            TaskModel task = getItem(position);
             // Check if an existing view is being reused, otherwise inflate the view
             ViewHolder viewHolder; // view lookup cache stored in tag
 
@@ -47,6 +52,8 @@ public class TaskFeedAdapter extends ArrayAdapter<TaskModel> {
 
                 viewHolder = new ViewHolder();
                 LayoutInflater inflater = LayoutInflater.from(getContext());
+
+                //Get all the components we need to modify
                 convertView = inflater.inflate(R.layout.listview_feed, parent, false);
                 viewHolder.title = (TextView) convertView.findViewById(R.id.title);
                 viewHolder.duration = (TextView) convertView.findViewById(R.id.duration);
@@ -62,20 +69,34 @@ public class TaskFeedAdapter extends ArrayAdapter<TaskModel> {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
 
+            //Display the data
 
-            viewHolder.title.setText(dataModel.getTitle());
-            viewHolder.description.setText(dataModel.getDescription());
-            viewHolder.creatorRating.setRating(dataModel.getCreatorRating());
+
+            viewHolder.title.setText(task.getTitle());
+            viewHolder.description.setText(task.getDescription());
+            //viewHolder.creatorRating.setRating(dataModel.getCreatorRating());
 
             //Change later
             viewHolder.duration.setText("20 mins ago");
-            viewHolder.distance.setText("1 mile away");
 
-            viewHolder.price.setText(Integer.toString(dataModel.getPrice()));
-            viewHolder.time.setText(dataModel.getTimeToComplete());
+            //Location
+            //Needs more accurate text descriptions, like feet
+            if(location != null) {
+                //Calculate the distance
+                float distance = location.distanceTo(task.getDropOffDestination());
+                //Meters to miles
+                int miles = (int) (distance * 0.000621371192);
+                viewHolder.distance.setText(miles + " miles away");
+            }
+
+
+            viewHolder.price.setText(Float.toString(task.getBaseCost()));
+            //viewHolder.time.setText(dataModel.getTimeToComplete());
 
             // Return the completed view to render on screen
             return convertView;
         }
+
+
 
 }
