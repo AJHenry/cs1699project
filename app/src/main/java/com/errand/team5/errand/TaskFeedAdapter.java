798e05f63727e1cce0d73d31571f29695a9220df
Andrew Henry <imandrewh@outlook.com>
2018-04-01 15:04:50 -0400

    Redid task view, added realtime data gathering from firebase to update feeds

--- app/src/main/java/com/errand/team5/errand/TaskFeedAdapter.java
+++ app/src/main/java/com/errand/team5/errand/TaskFeedAdapter.java
@@ -1,5 +1,6 @@
 package com.errand.team5.errand;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.location.Location;
 import android.view.LayoutInflater;
@@ -28,11 +29,10 @@ public class TaskFeedAdapter extends ArrayAdapter<TaskModel> {
         private static class ViewHolder {
             TextView title;
             TextView distance;
-            TextView duration;
             TextView price;
             TextView time;
             TextView description;
-            RatingBar creatorRating;
+            TextView timestamp;
             ImageView profileImage;
         }
 
@@ -58,13 +58,20 @@ public class TaskFeedAdapter extends ArrayAdapter<TaskModel> {
                 //Get all the components we need to modify
                 convertView = inflater.inflate(R.layout.listview_feed, parent, false);
                 viewHolder.title = (TextView) convertView.findViewById(R.id.title);
-                viewHolder.duration = (TextView) convertView.findViewById(R.id.duration);
                 viewHolder.distance = (TextView) convertView.findViewById(R.id.distance);
                 viewHolder.price = (TextView) convertView.findViewById(R.id.price);
                 viewHolder.time = (TextView) convertView.findViewById(R.id.time);
                 viewHolder.description = (TextView) convertView.findViewById(R.id.special_instructions);
-                viewHolder.creatorRating = (RatingBar) convertView.findViewById(R.id.creatorRating);
                 viewHolder.profileImage = (ImageView) convertView.findViewById(R.id.profile);
+                viewHolder.timestamp = (TextView) convertView.findViewById(R.id.listview_timestamp);
+
+                viewHolder.profileImage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //Show the profile fragment
+                        showProfile();
+                    }
+                });
 
                 convertView.setTag(viewHolder);
             } else {
@@ -79,7 +86,7 @@ public class TaskFeedAdapter extends ArrayAdapter<TaskModel> {
             //viewHolder.creatorRating.setRating(dataModel.getCreatorRating());
 
             //Change later
-            viewHolder.duration.setText("20 mins ago");
+            viewHolder.timestamp.setText(getTimeAgo(task.getPublishTime().getTime()));
 
             //mLocation
             //Needs more accurate text descriptions, like feet
@@ -100,5 +107,52 @@ public class TaskFeedAdapter extends ArrayAdapter<TaskModel> {
         }
 
 
+    public void showProfile(){
+        final Dialog dialog = new Dialog(getContext());
+        //dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.summary);
 
+        //Do processing here
+
+        dialog.show();
+
+    }
+
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+
+    public static String getTimeAgo(long time) {
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        // TODO: localize
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
+    }
 }
