f9fad765b5bbb6e628be791b33e8af8ca11cfdca
Andrew Henry <imandrewh@outlook.com>
2018-04-02 18:41:03 -0400

    Added better distance text formatting.

--- app/src/main/java/com/errand/team5/errand/mTimestamp.java
+++ app/src/main/java/com/errand/team5/errand/mTimestamp.java
@@ -1,5 +1,7 @@
 package com.errand.team5.errand;
 
+import java.time.Instant;
+
 /**
  * Created by Andrew on 3/31/2018.
  * Needed for the stupid no-argument constructor for Firebase
@@ -13,7 +15,9 @@ public class mTimestamp extends java.sql.Timestamp {
     }
 
     public mTimestamp() {
+        //Doesnt work with differnt time zones
         super(System.currentTimeMillis());
+        //super(Instant.now().toEpochMilli());
 
     }
 }
