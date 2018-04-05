78466dd574bc575d36cbd2f100726b3752bf79f6
Mohit Jain <mohitjain97@gmail.com>
2018-04-05 14:21:52 -0400

    Update Settings.java

--- app/src/main/java/com/errand/team5/errand/Settings.java
+++ app/src/main/java/com/errand/team5/errand/Settings.java
@@ -7,11 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 /**
  * TODO All of the settings
  */
 public class Settings extends Fragment {
+    private static String userIDs;
+    private static String electronicMails;
+    private static String photos;
+    private static String nameOfUsers;
+
 
 
     public Settings() {
@@ -24,6 +35,27 @@ public class Settings extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        userIDs = MainActivity.getUID();
+
+        DatabaseReference s1 = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference s2 = s1.child("errand-9c52d/users/");
+        s2.addValueEventListener(new ValueEventListener() {
+
+            public void onDataChange(DataSnapshot data) {
+                Long score = (Long) data.getValue();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+
+        });
+
     }
 
 }
