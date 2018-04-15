45d3448a919afa6076d1598f670c248c8c2997c3
Austin Marcus <aimarcus@andrew.cmu.edu>
2018-04-15 12:06:37 -0400

    skeleton for update receiver and update task

---
+++ app/src/main/java/com/errand/team5/errand/UpdateTask.java
@@ -0,0 +1,17 @@
+package com.errand.team5.errand;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Added by Austn M, 4/15/18
+ *
+ * Receives task fields from UpdateReceiver, finds matching task,
+ * replaces existing fields with all non-null input fields
+ */
+
+public class UpdateTask extends AppCompatActivity {
+
+    /**
+     * TODO: implement
+     */
+}
