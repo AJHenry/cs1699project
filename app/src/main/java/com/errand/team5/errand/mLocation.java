5a4b0c908e1c3a4f7ef7a5bb97a994efc0e72812
Andrew Henry <imandrewh@outlook.com>
2018-04-15 20:35:01 -0400

    Lots of changes, review files

--- app/src/main/java/com/errand/team5/errand/mLocation.java
+++ app/src/main/java/com/errand/team5/errand/mLocation.java
@@ -1,10 +1,12 @@
 package com.errand.team5.errand;
 
+import java.io.Serializable;
+
 /**
  * Created by Andrew on 3/31/2018.
  */
 
-public class mLocation extends android.location.Location {
+public class mLocation extends android.location.Location implements Serializable{
 
     public mLocation(){
         super("");
